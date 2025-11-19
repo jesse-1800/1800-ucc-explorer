a2514484f37d104c99d1e71414bd069c8f925336
jesse-1800 <jesse@1800copier.com>
2025-11-19 00:39:52 -0500

    .

--- app/models/IDProviderPartnerUsers.php
+++ app/models/IDProviderPartnerUsers.php
@@ -5,9 +5,9 @@ use Kernel\Database\QueryBuilder as Model;
 class IDProviderPartnerUsers extends Model
 {
     protected static $table = "idp_partner_users";
-    public static function fetch()
+    public function __construct($data = null)
     {
+        parent::__construct($data);
         Connection::initialize('identities');
-        return self::get();
     }
 }
