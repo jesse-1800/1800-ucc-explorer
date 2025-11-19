04dd93eb18fff108ee7bf1e96ddf2dd6ba36f7f7
jesse-1800 <jesse@1800copier.com>
2025-11-19 00:41:07 -0500

    .

--- app/models/IDProviderPartnerUsers.php
+++ app/models/IDProviderPartnerUsers.php
@@ -4,6 +4,8 @@ use Kernel\Database\QueryBuilder as Model;
 
 class IDProviderPartnerUsers extends Model
 {
+    // We're using multiple-database, WE MUST INSTANTIATE the class
+    // before we can use the static calls e.g. Model::get()
     protected static $table = "idp_partner_users";
     public function __construct($data = null)
     {
--- app/models/IDProviderPartners.php
+++ app/models/IDProviderPartners.php
@@ -3,8 +3,8 @@ use Kernel\Database\Connection;
 use Kernel\Database\QueryBuilder as Model;
 class IDProviderPartners extends Model
 {
-    // We're using multiple-database, WE MUST instantiate the class
-    // and NEVER use static calls e.g. Model::get() - DONT DO THIS
+    // We're using multiple-database, WE MUST INSTANTIATE the class
+    // before we can use the static calls e.g. Model::get()
     protected static $table = "idp_partners";
     public function __construct($data = null)
     {
