272f14a4096b7db049b40cc680003ccab985b0cf
jesse-1800 <jesse@1800copier.com>
2025-11-19 00:39:55 -0500

    .

--- app/models/IDProviderPartners.php
+++ app/models/IDProviderPartners.php
@@ -3,10 +3,12 @@ use Kernel\Database\Connection;
 use Kernel\Database\QueryBuilder as Model;
 class IDProviderPartners extends Model
 {
+    // We're using multiple-database, WE MUST instantiate the class
+    // and NEVER use static calls e.g. Model::get() - DONT DO THIS
     protected static $table = "idp_partners";
-    public static function fetch()
+    public function __construct($data = null)
     {
+        parent::__construct($data);
         Connection::initialize('identities');
-        return self::get();
     }
 }
