33b264702453e30e8809642bd1acd1c3b1d0a581
jesse-1800 <jesse@1800copier.com>
2025-11-19 02:23:42 -0500

    .

--- src/types/StoreTypes.ts
+++ src/types/StoreTypes.ts
@@ -20,6 +20,10 @@ export interface PartnerType {
   logo: string;
   phone_number: string;
   brand_color: string;
+  smtp_host: string;
+  smtp_port: string;
+  smtp_username: string;
+  smtp_password: string;
   supported_brands: any[];
   is_active: number;
 }
