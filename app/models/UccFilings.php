3b1fa9f26aea26664732999a281b940e76878082
jesse-1800 <jesse@1800copier.com>
2025-11-23 10:02:05 -0500

    .

---
+++ app/models/UccFilings.php
@@ -0,0 +1,13 @@
+<?php namespace App\Models;
+
+use Kernel\Database\QueryBuilder as Model;
+
+/**
+ * Class UccFilings
+ *
+ * @package App\Models
+ */
+class UccFilings extends Model
+{
+    protected static $table = "ucc_filings";
+}
