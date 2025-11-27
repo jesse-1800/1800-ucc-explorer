c583d517a8397ac0d00dae799480b65797ba0822
jesse-1800 <jesse@1800copier.com>
2025-11-27 09:11:24 -0500

    .

--- src/components/shared/ucc-filings/UccFilingsViewModal.vue
+++ src/components/shared/ucc-filings/UccFilingsViewModal.vue
@@ -1,5 +1,9 @@
 <template>
-  <MyModal color="transparent" v-model="modals.ucc_filing_viewer" title="UCC File Viewer">
+  <MyModal color="transparent" :fullscreen="fullscreen" v-model="modals.ucc_filing_viewer" title="UCC File Viewer">
+    <template #append-toolbar-icon>
+      <v-btn icon="mdi-open-in-new" :href="`/ucc-filing/${ucc_filing_id}`"></v-btn>
+      <v-btn icon="mdi-fullscreen" @click="fullscreen=!fullscreen"></v-btn>
+    </template>
     <UccFilingsViewer :ucc_file_id="ucc_filing_id"/>
   </MyModal>
 </template>
@@ -9,6 +13,7 @@ import {storeToRefs} from "pinia";
 import {GlobalStore} from "@/stores/globals";
 
 const store = GlobalStore();
+const fullscreen = ref(true);
 const {modals} = storeToRefs(store);
 const props = defineProps(['ucc_filing_id']);
 </script>
