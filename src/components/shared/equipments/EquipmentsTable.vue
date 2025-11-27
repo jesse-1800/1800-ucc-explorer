ab4b708f9a81a1e78d9d7a583892ef857f99fa38
jesse-1800 <jesse@1800copier.com>
2025-11-27 09:28:42 -0500

    .

---
+++ src/components/shared/equipments/EquipmentsTable.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-data-table
+    :headers="headers"
+    :items="equipments"
+    density="comfortable"
+    :style="theme_table_style">
+  </v-data-table>
+</template>
+
+<script lang="ts" setup>
+import {theme_table_style} from "@/composables/GlobalComposables";
+
+const props = defineProps(['equipments']);
+const headers = [
+  { title: 'Unit',         value: 'equipment_unit',     sortable: true },
+  { title: 'UCC Year',     value: 'equipment_ucc_year', sortable: true },
+  { title: 'Number',       value: 'equipment_number',   sortable: true },
+  { title: 'Brand',        value: 'equipment_brand',    sortable: true },
+  { title: 'Model',        value: 'equipment_model',    sortable: true },
+  { title: 'Description',  value: 'equipment_desc',     sortable: true },
+  { title: 'Code',         value: 'equipment_code',     sortable: true },
+  { title: 'Serial Number',value: 'equipment_serial_no',sortable: true },
+  { title: 'Size',         value: 'equipment_size',     sortable: true },
+  { title: 'End Year',     value: 'equipment_end_year', sortable: true },
+  { title: 'Value',        value: 'equipment_value',    sortable: true },
+]
+</script>
