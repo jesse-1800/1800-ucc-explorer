e355636e74d6103aa0a93ba3f849d853332cd857
jesse-1800 <jesse@1800copier.com>
2025-11-28 07:50:18 -0500

    .

--- src/pages/buyers.vue
+++ src/pages/buyers.vue
@@ -2,7 +2,12 @@
   <AppLayout>
     <template #title>Buyers</template>
     <template #content>
-      <v-data-table :items="ucc_buyers" :headers="headers"/>
+      <v-card :style="theme_card_style">
+        <v-card-text>
+          <TableSearch @click="Refresh"/>
+          <v-data-table :items="ucc_buyers" :headers="headers"/>
+        </v-card-text>
+      </v-card>
     </template>
   </AppLayout>
 </template>
@@ -10,9 +15,10 @@
 <script lang="ts" setup>
 import {storeToRefs} from "pinia";
 import {GlobalStore} from "@/stores/globals";
+import {theme_card_style} from "@/composables/GlobalComposables.ts";
+import {useAuth0} from "@auth0/auth0-vue";
 
 const store = GlobalStore();
-const {ucc_buyers} = storeToRefs(store);
 const headers = [
   { title: 'ID',                  value: 'id',                  sortable: true },
   { title: 'Buyer Company',       value: 'buyer_company',       sortable: true },
@@ -22,6 +28,11 @@ const headers = [
   { title: 'SIC Code',            value: 'buyer_sic',           sortable: true },
   { title: 'SIC Description',     value: 'buyer_sic_desc',      sortable: true },
   { title: 'DUNS Number',         value: 'buyer_duns',          sortable: true },
-]
-
+];
+const {ucc_buyers} = storeToRefs(store);
+const {getAccessTokenSilently} = useAuth0();
+const Refresh = async() => {
+  const token = await getAccessTokenSilently();
+  store.FetchAllData(token);
+}
 </script>
