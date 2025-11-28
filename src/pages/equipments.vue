469d4c847c6d5e60804ff7839df72e77569cc2c5
jesse-1800 <jesse@1800copier.com>
2025-11-28 07:37:32 -0500

    .

--- src/pages/equipments.vue
+++ src/pages/equipments.vue
@@ -2,7 +2,11 @@
   <AppLayout>
     <template #title>All Equipments</template>
     <template #content>
-      <EquipmentsTable :equipments="ucc_equipments"/>
+      <v-card :style="theme_card_style">
+        <v-card-text>
+          <EquipmentsTable :equipments="ucc_equipments"/>
+        </v-card-text>
+      </v-card>
     </template>
   </AppLayout>
 </template>
@@ -10,6 +14,7 @@
 <script lang="ts" setup>
 import {storeToRefs} from "pinia";
 import {GlobalStore} from "@/stores/globals";
+import {theme_card_style} from "@/composables/GlobalComposables.ts";
 
 const store = GlobalStore();
 const {ucc_equipments} = storeToRefs(store);
