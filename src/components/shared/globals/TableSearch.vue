97063f3b17093821da4e6954436df766a38679f1
jesse-1800 <jesse@1800copier.com>
2025-11-28 07:46:20 -0500

    .

---
+++ src/components/shared/globals/TableSearch.vue
@@ -0,0 +1,32 @@
+<template>
+  <flexed-between class="mb-5">
+    <div>
+      <v-text-field
+        width="250"
+        hide-details
+        density="compact"
+        variant="outlined"
+        v-model="table_search"
+        prepend-inner-icon="mdi-magnify"
+        @click:append-inner="table_search=''"
+        :append-inner-icon="table_search?'mdi-close':''">
+      </v-text-field>
+    </div>
+    <v-spacer/>
+    <v-btn
+      text="Refresh"
+      color="primary"
+      @click="$emit('refresh')"
+      prepend-icon="mdi-refresh">
+    </v-btn>
+    <slot></slot>
+  </flexed-between>
+</template>
+
+<script setup>
+import {storeToRefs} from "pinia";
+import {GlobalStore} from "@/stores/globals";
+
+const store = GlobalStore();
+const {table_search} = storeToRefs(store);
+</script>
