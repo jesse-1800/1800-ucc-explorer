1564c9f17178ba7232982b71f779442a2bca14b0
Jesse Strife <jesse@1800copier.com>
2025-12-02 21:33:13 -0500

    .

--- app/controllers/UccfilingsController.php
+++ app/controllers/UccfilingsController.php
@@ -10,10 +10,12 @@ class UccfilingsController
         $page_size = (int)($_POST['page_size'] ?? 100);
         $offset    = ($curr_page - 1) * $page_size;
         $sort_map = array(
-            'id'            => 'UF.id',
-            'ucc_date'      => "STR_TO_DATE(UF.ucc_date,'%m/%d/%Y')",
-            'ucc_status'    => 'UF.ucc_status',
-            'buyer_company' => 'UB.buyer_company',
+            'id'              => 'UF.id',
+            'ucc_date'        => "STR_TO_DATE(UF.ucc_date,'%m/%d/%Y')",
+            'ucc_status'      => 'UF.ucc_status',
+            'buyer_company'   => 'buyer_company',
+            'buyer_state'     => 'buyer_state',
+            'equipment_count' => 'equipment_count',
         );
         $sort_by   = $sort_map[$_POST['sort_by']] ?? 'id';
         $order_by  = $_POST['order_by'];
@@ -27,11 +29,13 @@ class UccfilingsController
         $provider_id  = trim($_POST['provider_id'] ?? '');
         $assignee_id  = trim($_POST['assignee_id'] ?? '');
         $ucc_status   = trim($_POST['ucc_status'] ?? '');
+        $buyer_state  = trim($_POST['buyer_state'] ?? '');
 
         if($search      !== ""){
             $search_input = addslashes($search);
             $where[] = ("
-                (UB.buyer_company LIKE '%$search_input%'
+                (buyer_company LIKE '%$search_input%'
+                OR buyer_state LIKE '%$search_input%'
                 OR UF.ucc_status LIKE '%$search_input%'
                 OR UF.id LIKE '%$search_input%')
             ");
@@ -52,10 +56,14 @@ class UccfilingsController
         if($ucc_status  !== ""){
             $where[] = "UF.ucc_status = '$ucc_status'";
         }
+        if($buyer_state !== ""){
+            $where[] = "UB.buyer_state = '$buyer_state'";
+        }
 
         $where_sql = count($where) ? "WHERE " . implode(" AND ", $where) : "";
+        $equipment_count = "(SELECT COUNT(*) FROM ucc_equipments EQ WHERE EQ.ucc_filing_id = UF.id)";
         $result = $db->query("
-          SELECT UF.*,UB.buyer_company AS buyer_company,(SELECT COUNT(*) FROM ucc_equipments EQ WHERE EQ.ucc_filing_id = UF.id) AS equipment_count
+          SELECT UF.*,UB.buyer_company,UB.buyer_state,$equipment_count AS equipment_count
           FROM ucc_filings UF
           LEFT JOIN ucc_buyers UB ON UF.buyer_id = UB.id
           $where_sql
