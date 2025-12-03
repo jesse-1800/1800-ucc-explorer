232cfbf8ac157300a78437ae3d5609b94619aa37
jesse-1800 <jesse@1800copier.com>
2025-12-03 13:39:49 -0500

    .

--- app/controllers/DataController.php
+++ app/controllers/DataController.php
@@ -34,8 +34,8 @@ class DataController {
             // UCC Data
             'ucc_files'         => UccFileManager::where('partner_id',$partner_id)->get(),
             'ucc_map_columns'   => json_decode(file_get_contents("$path/ucc_filings.json")),
-            'ucc_statuses'      => UccFilings::select('ucc_status')->distinct('ucc_status')->get(),
-            'ucc_cities'        => UccBuyers::select('buyer_city')->distinct('buyer_city')->get(),
+            'ucc_statuses'      => UccFilings::distinct('ucc_status')->get(),
+            'ucc_cities'        => UccBuyers::distinct('buyer_city')->get(),
             'ucc_assignees'     => UccAssignees::select('id,assignee_company')->where('partner_id',$partner_id)->get(),
             'ucc_providers'     => UccProviders::select('id,provider_company')->where('partner_id',$partner_id)->get(),
         );
