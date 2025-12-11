dedbde125baae44213a5aad484ddd6bc90ab4dc4
jesse-1800 <jesse@1800copier.com>
2025-12-10 23:10:22 -0500

    .

---
+++ src/components/shared/shared-forms/BuyerForm.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-row v-if="buyer">
+    <v-col cols="12">
+      <v-text-field :hide-details="condensed" variant="underlined" label="Company" :model-value="buyer.buyer_company" readonly/>
+    </v-col>
+    <v-col cols="12">
+      <v-text-field :hide-details="condensed" variant="underlined" label="Address" :model-value="buyer.buyer_adress1" readonly/>
+    </v-col>
+    <v-col cols="12" sm="4">
+      <v-text-field :hide-details="condensed" variant="underlined" label="City" :model-value="buyer.buyer_city" readonly/>
+    </v-col>
+    <v-col cols="12" sm="4">
+      <v-text-field :hide-details="condensed" variant="underlined" label="State" :model-value="buyer.buyer_state" readonly/>
+    </v-col>
+    <v-col cols="12" sm="4">
+      <v-text-field :hide-details="condensed" variant="underlined" label="ZIP" :model-value="buyer.buyer_zip" readonly/>
+    </v-col>
+    <v-col cols="12">
+      <v-text-field :hide-details="condensed" variant="underlined" label="Phone" :model-value="buyer.buyer_phone" readonly/>
+    </v-col>
+  </v-row>
+</template>
+
+<script setup>
+const {buyer} = defineProps(['buyer','condensed'])
+</script>
