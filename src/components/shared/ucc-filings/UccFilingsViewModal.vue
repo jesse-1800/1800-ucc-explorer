2d4fcd8e8c3779af1ca9503b1e149e74d7e59ef8
jesse-1800 <jesse@1800copier.com>
2025-12-10 23:25:19 -0500

    .

--- src/components/shared/ucc-filings/UccFilingsViewModal.vue
+++ src/components/shared/ucc-filings/UccFilingsViewModal.vue
@@ -1,11 +1,18 @@
 <template>
-  <MyModal color="transparent" max_width="1024" :fullscreen="fullscreen" v-model="modals.ucc_filing_viewer" title="UCC File Viewer">
+  <MyModal
+    max_width="1024"
+    color="transparent"
+    title="UCC File Viewer"
+    :fullscreen="fullscreen"
+    v-model="modals.ucc_filing_viewer">
+
     <template #append-toolbar-icon>
-      <v-btn icon="mdi-open-in-new" :href="`/ucc-filing/${ucc_filing_id}`"></v-btn>
+      <v-btn icon="mdi-open-in-new" :href="`/ucc-filing/${view_ucc_id}`"></v-btn>
       <v-btn icon="mdi-fullscreen" @click="fullscreen=!fullscreen"></v-btn>
     </template>
-    <UccFilingsViewer :ucc_filing_id="ucc_filing_id"/>
+    <UccFilingsViewer :view_ucc_id="view_ucc_id"/>
     <div class="mb-10"></div>
+
   </MyModal>
 </template>
 
@@ -15,6 +22,5 @@ import {GlobalStore} from "@/stores/globals";
 
 const store = GlobalStore();
 const fullscreen = ref(false);
-const {modals} = storeToRefs(store);
-const props = defineProps(['ucc_filing_id']);
+const {modals,view_ucc_id} = storeToRefs(store);
 </script>
