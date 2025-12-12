87795a12fea66b477d3c6defefd797777fef6c9a
jesse-1800 <jesse@1800copier.com>
2025-12-12 11:15:49 -0500

    .

--- app/controllers/ContactsController.php
+++ app/controllers/ContactsController.php
@@ -1,8 +1,61 @@
 <?php
 
 use App\Models\UccContacts;
+use Kernel\Database\Database;
 
 class ContactsController {
+    public function paginate($partner_id)
+    {
+        $database  = new Database();
+        $curr_page = (int)($_POST['curr_page'] ?? 1);
+        $page_size = (int)($_POST['page_size'] ?? 100);
+        $offset    = ($curr_page - 1) * $page_size;
+        $sort_map  = array(
+            'id'      => 'CT.id',
+            'fullname'=> 'fullname',
+            'company' => 'UB.buyer_company',
+            'title'   => 'CT.title',
+            'email'   => 'CT.email',
+        );
+        $sort_by   = $sort_map[$_POST['sort_by']] ?? 'id';
+        $order_by  = $_POST['order_by'];
+        $total     = $database->row("SELECT COUNT(*) AS 'total' FROM ucc_contacts WHERE partner_id='$partner_id'")->total;
+
+        // where queries
+        $where  = array();
+        $search = trim($_POST['search'] ?? '');
+
+        if ($partner_id !== "") {
+            $where[] = "CT.partner_id = '$partner_id'";
+        }
+        if ($search !== "") {
+            $search_input = addslashes($search);
+            $where[] = ("
+                (
+                    id LIKE '%$search_input%' OR
+                    name LIKE '%$search_input%' OR
+                    company LIKE '%$search_input%' OR
+                    title LIKE '%$search_input%' OR
+                    email LIKE '%$search_input%'
+                )
+            ");
+        }
+
+        $where_sql = count($where) ? "WHERE ".implode(" AND ", $where) : "";
+        $result = $database->query("
+          SELECT CT.*,CONCAT(CT.firstname,' ',CT.lastname) 
+          AS fullname,UB.buyer_company,UB.id AS buyer_id
+          FROM ucc_contacts CT
+          LEFT JOIN ucc_buyers UB ON CT.buyer_id = UB.id
+          $where_sql
+          ORDER BY $sort_by $order_by
+          LIMIT $page_size
+          OFFSET $offset;
+        ");
+
+        return json(['items' => $result, 'total' => $total]);
+    }
+
     public function store()
     {
         $contact = json_decode($_POST['contact']);
