3fd94a88abc1ececa5b59e1483c10ddd89bea527
Jesse Strife <jesse@1800copier.com>
2025-12-13 21:25:23 -0500

    .

--- src/components/shared/buyers/UccBuyerViewer.vue
+++ src/components/shared/buyers/UccBuyerViewer.vue
@@ -3,8 +3,20 @@
     v-if="buyer"
     max_width="800"
     color="transparent"
-    v-model="modals.customer_profile"
-    :title="buyer.buyer_company ?? 'Customer Profile'">
+    v-model="modals.customer_profile">
+    <template #title>
+      <Flexed-Between>
+        <div>{{buyer?.buyer_company ?? 'Customer Profile'}}</div>
+        <v-btn
+          text="Enrich"
+          @click="EnrichBuyer"
+          prepend-icon="mdi-radar"
+          :loading="is_loading"
+          variant="elevated"
+          color="primary">
+        </v-btn>
+      </Flexed-Between>
+    </template>
     <v-expansion-panels :model-value="0" elevation="0">
       <panel icon="mdi-domain" :title="`Buyer Profile`">
         <v-card-text class="border">
@@ -23,14 +35,17 @@
 </template>
 
 <script lang="ts" setup>
+import Axios from "axios";
 import {storeToRefs} from "pinia";
 import {useAuth0} from "@auth0/auth0-vue";
 import {GlobalStore} from "@/stores/globals";
 import {UccServer} from "@/plugins/ucc-server";
+import type {UccBuyersType} from "@/types/StoreTypes";
 
-const buyer = ref(null);
 const contacts = ref([]);
 const ucc_filings = ref([]);
+const is_loading = ref(false);
+const buyer = ref<UccBuyersType|null>(null);
 
 const {modals} = storeToRefs(GlobalStore());
 const {getAccessTokenSilently} = useAuth0();
@@ -42,6 +57,27 @@ const FindBuyerProfile = async () => {
     buyer.value = res.data.buyer;
     contacts.value = res.data.contacts;
     ucc_filings.value = res.data.ucc_filings;
+  });
+}
+const EnrichBuyer = async () => {
+  is_loading.value = true;
+  const form = new FormData;
+  const webhook = "https://connect.pabbly.com/workflow/sendwebhookdata/IjU3NjcwNTZkMDYzNTA0MzM1MjZlNTUzNzUxMzYi_pc";
+  const new_contacts = [...contacts.value] as any;
+  if (new_contacts.length == 1) {
+    new_contacts.push(<any>{
+      id: "",
+      firstname: "",
+      lastname: "",
+    });
+  }
+  form.append('contacts',JSON.stringify(new_contacts));
+  form.append('buyer_data',JSON.stringify(buyer.value));
+  Axios.post(webhook,form).then(res => {
+    console.log(res.data);
+  }).finally(() => {
+    is_loading.value = false;
+    FindBuyerProfile();
   });
 }
 
